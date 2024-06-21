dbbf9441e137175b7463ef291d2cb6e65828055a
Ishanepzy <issann599@gmail.com>
2024-06-21 20:06:22 +0545

    get result back from previous activity

--- app/src/main/java/com/example/mobileprogramming/unit_4/About.java
+++ app/src/main/java/com/example/mobileprogramming/unit_4/About.java
@@ -1,10 +1,12 @@
 package com.example.mobileprogramming.unit_4;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +19,8 @@ public class About extends AppCompatActivity {
     public  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+
+        Resources res = getResources();
 
         TextView TextView = findViewById(R.id.aboutFullNameValue);
         TextView Gender = findViewById(R.id.aboutGenderValue);
@@ -34,6 +38,14 @@ public class About extends AppCompatActivity {
         Country.setText(country);
         Sports.setText(sports.toString());
 
+        if (sports != null && !sports.isEmpty()) {
+            String joinedSports = String.join(", ", sports);
+            Sports.setText(joinedSports);
+        } else {
+            Toast.makeText(getApplicationContext(), "No sports found", Toast.LENGTH_SHORT).show();
+            Sports.setText(res.getString(R.string.na));
+        }
+
         Button backButton = findViewById(R.id.aboutBackButton);
 
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -44,7 +56,16 @@ public class About extends AppCompatActivity {
             }
         });
 
+        Button activityDialogButton = findViewById(R.id.aboutDialogButton);
+
+        activityDialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.putExtra("data", "Hello World");
+                setResult(200, i);
+                finish();
+            }
+        });
     }
-
-
 }
--- app/src/main/java/com/example/mobileprogramming/unit_4/Home.java
+++ app/src/main/java/com/example/mobileprogramming/unit_4/Home.java
@@ -1,6 +1,7 @@
 package com.example.mobileprogramming.unit_4;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,7 +18,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.mobileprogramming.R;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Home extends AppCompatActivity {
 
@@ -26,6 +26,8 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.constraint_layout);
 
+        Resources res = getResources();
+
         Spinner countrySpinner = findViewById(R.id.countrySpinner);
 
         String[] countryList = {"Nepal", "Korea", "Netherlands", "United Kingdom", "Germany"};
@@ -33,11 +35,11 @@ public class Home extends AppCompatActivity {
         ArrayAdapter<String> countryAdapter = new ArrayAdapter<>(this, R.layout.spinner_item, countryList);
         countrySpinner.setAdapter(countryAdapter);
 
-        TextView headingText = findViewById(R.id.headingText);
+        TextView fullName = findViewById(R.id.headingText);
         Button submitButton = findViewById(R.id.submit_button);
         Button cancelButton = findViewById(R.id.cancel_button);
-        TextView editText = findViewById(R.id.editText);
         RadioGroup genderGroup = findViewById(R.id.genderGroup);
+        TextView fullNameText = findViewById(R.id.fullNameValue);
 
         CheckBox footballCheckbox = findViewById(R.id.checkboxFootball);
         CheckBox basketballCheckbox = findViewById(R.id.checkboxBasketball);
@@ -58,11 +60,15 @@ public class Home extends AppCompatActivity {
                 Intent intent = new Intent(Home.this, About.class);
                 startActivity(intent);
 
-                String fullName = editText.getText().toString();
+                String fullName = fullNameText.getText().toString();
 
                 int checkedRadioButtonId = genderGroup.getCheckedRadioButtonId();
 
                 ArrayList<String> selectedSports = new ArrayList<>();
+
+                String fullNameText = "";
+                if (fullName != null && !fullName.isEmpty()) fullNameText = fullName;
+                else fullNameText = res.getString(R.string.anonymous);
 
                 String selectedGender = "unknown";
                 if (checkedRadioButtonId == R.id.genderMale)
@@ -78,16 +84,40 @@ public class Home extends AppCompatActivity {
 
                 Intent i = new Intent(Home.this, About.class);
 
-                i.putExtra("fullName", "Ishan Pradhananga");
+                i.putExtra("fullName", fullNameText);
                 i.putExtra("gender", selectedGender);
                 i.putExtra("country", "Nepal");
                 i.putStringArrayListExtra("sports", selectedSports);
 
                startActivity(i);
-
-
+               startActivityForResult(i,1);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 20){
+            Log.d("requestCode","200");
+            if(resultCode == 200){
+
+                if(data!=null){
+                    String result = data.getStringExtra("data");
+                    //View.setText(result);
+                    Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+        if(requestCode == 20){
+            if(resultCode == 201){
+                if(data!=null){
+                    //View.setText("");
+                    Toast.makeText(getApplicationContext(), " ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 
     @Override
@@ -127,169 +157,3 @@ public class Home extends AppCompatActivity {
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//package com.example.myapplicationmp.unit_3;
-//
-//import android.app.Activity;
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.util.Log;
-//import android.view.View;
-//import android.widget.ArrayAdapter;
-//import android.widget.Button;
-//import android.widget.CheckBox;
-//import android.widget.EditText;
-//import android.widget.RadioGroup;
-//import android.widget.Spinner;
-//import android.widget.TextView;
-//
-//import com.example.myapplicationmp.unit_2.Unit_2;
-//import com.example.myapplicationmp.unit_4.About;
-//import com.google.android.material
-//        .snackbar
-//        .Snackbar;
-//
-//import android.widget.Toast;
-//
-//import com.example.myapplicationmp.R;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//
-//public class Unit_3 extends Activity {
-//
-//    private Button submitBtn;
-//
-//    private Button cancel_button;
-//    private TextView headingText;
-//    private EditText editText;
-//
-//    private RadioGroup genderGroup;
-//
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        Log.d("myStateLog", "Home-onStart");
-//    }
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        Log.d("myStateLog", "Home-onResume");
-//        Toast.makeText(getApplicationContext(), "Resumed", Toast.LENGTH_SHORT).show();
-//    }
-//
-//    @Override
-//    protected void onRestart() {
-//        super.onRestart();
-//        Log.d("myStateLog", "Home-onStart");
-//    }
-//
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//        Log.d("myStateLog", "Home-OnPause");
-//    }
-//
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        Log.d("myStateLog", "Home-onDestroy");
-//    }
-//
-//    @Override
-//    protected void onStop() {
-//        super.onStop();
-//        Log.d("myStateLog", "Home-onStop");
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle b) {
-//        super.onCreate(b);
-//        setContentView(R.layout.unit_3_constraint);
-//
-//        Log.d("myStateLog", "Home-onCreate");
-//
-//        Spinner country_spinner = findViewById(R.id.country_spinner);
-//        String[] countryList = {"Korea", "Netherlands", "United Kingdom", "Germany"};
-//        ArrayAdapter<String> countryAdapter = new ArrayAdapter<>(this, R.layout.unit_3_spinner_item, countryList);
-//        country_spinner.setAdapter(countryAdapter);
-//
-//
-//        headingText = findViewById(R.id.heading_text);
-//        submitBtn = findViewById(R.id.submit_button);
-//        editText = findViewById(R.id.edit_text);
-//        cancel_button = findViewById(R.id.cancel_button);
-//        genderGroup = findViewById(R.id.radioGroup);
-//
-//        CheckBox footballCheckbox = findViewById(R.id.checkbox_football);
-//        CheckBox basketballCheckbox = findViewById(R.id.checkbox_basketball);
-//        CheckBox cricketCheckbox = findViewById(R.id.checkbox_cricket);
-//        CheckBox judoCheckbox = findViewById(R.id.checkbox_judo);
-//
-//
-//        cancel_button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // Create a snackbar
-//                Snackbar snackbar = Snackbar.make(v, "Cancelled", Snackbar.LENGTH_LONG);
-//                snackbar.show();
-//
-//
-//                // Toast.makeText(getApplicationContext(), "Cancelled", Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
-//
-//
-////
-//        submitBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String selectedGender = "unknown";
-//
-//                Intent i = new Intent(Unit_3.this, About.class);
-////                boolean keyboard =keyboardUtil.isKeyboardVisible;
-//
-//                ArrayList<String> selectedSports = new ArrayList<>();
-//                int checkedRadioButtonId = genderGroup.getCheckedRadioButtonId();
-//
-//                if (checkedRadioButtonId == R.id.radioMale)
-//                    selectedGender = "Male";
-//                else selectedGender = "Female";
-//
-//                CheckBox[] checkBoxes = {footballCheckbox, basketballCheckbox, cricketCheckbox, judoCheckbox};
-//
-//                for (CheckBox checkBox : checkBoxes) {
-//                    if (checkBox.isChecked()) selectedSports.add(checkBox.getText().toString());
-//                }
-//
-//
-//                i.putExtra("gender", selectedGender);
-//                i.putExtra("country", "Nepal");
-//                i.putExtra("fullName", "Prajwal Khatiwada");
-//                i.putExtra("sports", selectedSports);
-//
-//                startActivity(i);
-//            }
-//        });
-//    }
-//}
